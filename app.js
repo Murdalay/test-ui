aa99f9e183a0b413c7a01dd1a838ea7861907496
Ragnar <murdalay@gmail.com>
2015-08-31 15:18:10 +0300

    mobile detection added

--- app.js
+++ app.js
@@ -5,12 +5,16 @@
 		app = express(),
 		basePath = process.cwd(),
 		pathToMobile = path.join(basePath, 'mobile'),
+		pathToStatic = path.join(basePath, 'static'),
 		pathToDesktop = path.join(basePath, 'desktop');
 
 	app
 	    .disable('x-powered-by')
 	    .use(morgan('dev'));
-	app .use('/', express.static(basePath));
+	app .use('/', express.static(basePath))
+		.use('/desktop/', express.static(pathToDesktop))
+		.use('/mobile/', express.static(pathToDesktop))
+		.use('/static/', express.static(pathToStatic));
 
 
 app.listen(1111);	
